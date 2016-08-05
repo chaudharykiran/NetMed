a987f5ee5b2e17908599f6c1c92a552d0e37d14b
Kiran Kumar Chaudhary <hellokiran2020@gmail.com>
2016-08-05 23:43:53 +0545

    Index view added.

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
 from django.contrib import admin
 
+from .views import *
+
 urlpatterns = [
+	url(r'^', Index.as_view(), name='index'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.template import RequestContext
+
+from django.views.generic import View
 
 # Create your views here.
+# view for main home page.
+class Index(View):
+	"""Handle get and post request separately for Index page"""
+	template_name = 'home/index.html'
+	context = {'title' : 'Home'}
+
+	def get(self, request, *args, **kwargs):
+		return render(request,
+						self.template_name,
+						self.context)
