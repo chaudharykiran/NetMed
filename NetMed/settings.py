497f3e8ad676a9bb0ee80289f2d38be970bb6886
Kiran Kumar Chaudhary <hellokiran2020@gmail.com>
2016-08-07 18:39:43 +0545

    App improved for deployment.

--- NetMed/settings.py
+++ NetMed/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(___file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,14 +124,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+#STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticfiles")
 
 STATIC_URL = '/static/'
 
-# Static Files Directories
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "static"),
+        os.path.join(PROJECT_ROOT, "static"),
         ]
 
 # Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
--- NetMed/wsgi.py
+++ NetMed/wsgi.py
@@ -11,7 +11,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NetMed.settings")
 
