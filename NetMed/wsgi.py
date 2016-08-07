35cefdc9c9914cf0427baab20d0efd79597377a3
Kiran Kumar Chaudhary <hellokiran2020@gmail.com>
2016-08-07 14:34:15 +0545

    App prepared to deployment in heroku.

--- NetMed/wsgi.py
+++ NetMed/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "NetMed.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn NetMed.wsgi --log-file -
