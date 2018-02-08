6cda3d37b5f23e28d8c1ab2951d6655d88dc1691
Kiran Kumar Chaudhary <hellokiran2020@gmail.com>
2018-02-08 12:36:49 +0545

    Initial commit

--- NetMed/settings.py
+++ NetMed/settings.py
@@ -91,18 +91,18 @@ DATABASES = {
 #    }
 }
 
-urlparse.uses_netloc.append("postgres")
-url = urlparse.urlparse(os.environ["DATABASE_URL"])
+#urlparse.uses_netloc.append("postgres")
+#url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
-conn = psycopg2.connect(
-    database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port
-)
+#conn = psycopg2.connect(
+#    database=url.path[1:],
+#    user=url.username,
+#    password=url.password,
+#    host=url.hostname,
+#    port=url.port
+#)
 
-DATABASES['default'] = dj_database_url.config()
+#DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
